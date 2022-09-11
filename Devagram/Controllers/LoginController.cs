109df41354c73f9cdcd46664bab64bf0df7fb2b9
Lucas Pasche <lucas.carmo@meta.com.br>
2022-09-11 11:46:59 -0300

    início da API de login feito

---
+++ Devagram/Controllers/LoginController.cs
@@ -0,0 +1,38 @@
+﻿using Devagram.DTOs;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Devagram.Controllers
+{
+    [ApiController]
+    [Route("api/[Controller]")] // puxa o nome do controller se tiver nesse padrao. no caso de login,
+                                // fica api/Login
+    public class LoginController : ControllerBase
+    {
+        // registro de logs
+        private readonly ILogger<LoginController> _logger;
+        public LoginController(ILogger<LoginController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public IActionResult Login([FromBody] LoginDTO loginData)
+        {
+            try
+            {
+                throw new ArgumentException("Erro ao preencher os dados");
+            }
+            catch(Exception error)
+            {
+                _logger.LogError($"Ocorreu um erro no login: {error.Message}");
+                return StatusCode(StatusCodes.Status500InternalServerError, new ErrorResponseDTO()
+                {
+                    Status = StatusCodes.Status500InternalServerError,
+                    Message = "Ocorreu um erro ao fazer o login."
+                });
+            }
+        }
+    }
+}
---
+++ Devagram/DTOs/ErrorResponseDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Devagram.DTOs
+{
+    public class ErrorResponseDTO
+    {
+        public int Status { get; set; }
+
+        public string Message { get; set; }
+    }
+}
